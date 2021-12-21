aad075d37e63a8464efe888d23c6e01565be32c0
arunvinu <arunkumarxworkz@gmail.com>
2021-12-21 17:31:46 +0545

    view1

---
+++ Xphoned.java
@@ -0,0 +1,8 @@
+class Xphoned{
+
+public static void main (String [ ]args){
+double [ ] xgraph = { 9.59176523658896523147d,78.1696045665d,779.595209665444d,8.951255668556658963d};
+System.out.println(xgraph [ 0 ]);
+System.out.println(xgraph [ 3 ]);
+}
+}
